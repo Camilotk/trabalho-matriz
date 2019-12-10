356302bf84ea86e7dc382cc1c5001205d744026d
Camilo Cunha de Azevedo <camilotk@gmail.com>
2019-12-10 17:59:02 -0300

    .

--- views/template/layout.php
+++ views/template/layout.php
@@ -1,63 +1,123 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html>
+
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
     <title><?=$this->e($title)?></title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="/assets/css/style.css">
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
 </head>
+
 <body>
-<nav class="navbar navbar-expand-sm navbar-dark bg-success">
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div class="sidebar-header" style="display: flex;">
+                <img src="/assets/img/logo_ifrs_b.png" style="max-width: 26px; max-height: 35px" />
+                <h3 style="margin-left: 15px;">IFRS</h3>
+            </div>
+
+            <ul class="list-unstyled components">
+                <p>Sistema de cadastro de horários de Docentes e Disciplinas</p>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li >
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Cadastro</a>
+                    <ul class="collapse list-unstyled" id="homeSubmenu">
+                        <li>
+                            <a href="/curso">Curso</a>
+                        </li>
+                        <li>
+                            <a href="/componente">Componente</a>
+                        </li>
+                        <li>
+                            <a href="/docente">Docente</a>
+                        </li>
+                        <li>
+                            <a href="/turma">Turma</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Listagem</a>
+                    <ul class="collapse list-unstyled" id="pageSubmenu">
+                        <li>
+                            <a href="/curso/listagem">Curso</a>
+                        </li>
+                        <li>
+                            <a href="/componente/listagem">Componente</a>
+                        </li>
+                        <li>
+                            <a href="/docente/listagem">Docente</a>
+                        </li>
+                        <li>
+                            <a href="/turma/listagem">Turma</a>
+                        </li>
+                    </ul>
+                </li>
+                <?php
+                /*
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+                 */?>
+            </ul>
+
+            <ul class="list-unstyled CTAs">
+                <?php /*
+                <li>
+                    <a href="#" class="download">Download source</a>
+                </li> */?>
+                <li>
+                    <a href="https://ifrs.edu.br/bento/" class="article">Ir pro site do IFRS</a>
+                </li>
+            </ul>
+        </nav>
+
+        <!-- Page Content  -->
+        <div id="content">
+            <nav class="navbar navbar-expand-sm  navbar-dark bg-success">
+                    <button type="button" id="sidebarCollapse" class="btn btn-success">
+                        <i class="fas fa-align-left"></i>
+                    </button>
   <a class="navbar-brand" href="#">IFRS</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
     <span class="navbar-toggler-icon"></span>
   </button>
-
-  <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="/">Home</a>
-      </li>
-<?php /* <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-        </li> */ ?>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Cadastros
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="/curso">Curso</a>
-          <a class="dropdown-item" href="/componente">Componente</a>
-          <a class="dropdown-item" href="/docente">Docente</a>
-          <a class="dropdown-item" href="/turma">Turma</a>
-        </div>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Listagens 
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="/curso/listagem">Curso</a>
-          <a class="dropdown-item" href="/componente/listagem">Componente</a>
-          <a class="dropdown-item" href="/docente/listagem">Docente</a>
-          <a class="dropdown-item" href="/turma/listagem">Turma</a>
-        </div>
-      </li>
-<?php /* <li class="nav-item">
-        <a class="nav-link disabled" href="#">Desativado</a>
-        </li> */?>
-    </ul>
-  </div>
 </nav>
-</head>
+   <?=$this->section('content');?>
+        </div>
+    </div>
 
-<?=$this->section('content');?>
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+<script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
-    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-<?=$this->section('script');?>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+    </script>
+    <?=$this->section('script');?>
 </body>
+
 </html>
